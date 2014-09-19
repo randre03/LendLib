011ceb0c95abf34f66747c80387c7ba39f113e73
randre03 <roger.randall@gmail.com>
2014-09-19 00:28:38 -0500

    fixed all known errors

--- LendLib.js
+++ LendLib.js
@@ -1,21 +1,7 @@
 lists = new Meteor.Collection('lists'); //new list
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
 
-  //Template.hello.helpers({
-  //  counter: function () {
-  //    return Session.get("counter");
-  //  }
-  //});
-  //
-  //Template.hello.events({
-  //  'click button': function () {
-  //    // increment the counter when button is clicked
-  //    Session.set("counter", Session.get("counter") + 1);
-  //  }
-  //});
     Template.categories.lists = function () {
       return lists.find({}, {sort: {Category: 1}});
     };
@@ -34,8 +20,8 @@ if (Meteor.isClient) {
             focusText(t.find("#add-category")); //sets the focus on the input box
         },
 
-        'keyup #add-category': function (e, t) {
-            if(e === 13) {
+        'keyup #add-category': function (e,t) {
+            if(e.which === 13) {
                 var catVal = String(e.target.value || ''); //checks to see if the input field has anything in it
                 if(catVal) {
                     lists.insert({Category: catVal}); //inserts the value from the input field into the list
@@ -54,24 +40,24 @@ if (Meteor.isClient) {
 
     /////Generic Helper Functions\\\\\
     //this function positions the cursor where it should be
-    function focusText(i, val) {
+    function focusText(i,val) {
         i.focus();
-        i.value = val ? val : '';
+        i.value = val ? val : "";
         i.select();
     }
 
-    function selectCategory(e, t) {
+    function selectCategory(e,t) {
         Session.set('current_list', this._id);
     }
 
-    function addItems(list_id, item_name) {
-        if (!item_name && !List_id)
+    function addItem(list_id, item_name) {
+        if (!item_name && !list_id)
             return;
         lists.update({_id: list_id}, {$addToSet: {items: {Name: item_name}}});
     }
 
     function removeItem(list_id, item_name) {
-        if (!item_name && !List_id)
+        if (!item_name && !list_id)
             return;
         lists.update({_id: list_id}, {$pull: {items: {Name: item_name}}});
     }
@@ -81,22 +67,23 @@ if (Meteor.isClient) {
         if(l && l.items) {
             for(var i=0; i< l.items.length; i++) {
                 if(l.items[i].Name === item_name) {
-                    l.items[i].LentTo = lendee_name;
+                    l.items[i].lentTo = lendee_name;
                 }
             }
-            lists.update({"_id": list_id}, {$set:{"items":l.items}});
+            lists.update({"_id": list_id}, {$set: {"items": l.items}});
         }
     }
 
     Template.list.items = function () {
-        if(Session.equals('current_list', null)) return null; //is a list selected
+        if(Session.equals('current_list', null))
+          return null; //is a list selected
         else {
             var cats = lists.findOne({_id: Session.get('current_list')}); //cats is short for the 'category'
             if(cats && cats.items) { //make sure something exists before doing work
                 for (var i=0; i<cats.items.length; i++) {
                     var d = cats.items[i];
-                    d.Lendee = d.LentTo ? d.lentTo : "free";
-                    d.LendClass = d.LentTo ? "label-important" : "label-success";
+                    d.Lendee = d.lentTo ? d.lentTo : "free";
+                    d.LendClass = d.lentTo ? "label-important" : "label-success";
                 }
                 return cats.items;
             }
@@ -123,8 +110,8 @@ if (Meteor.isClient) {
     };
 
     Template.list.events({
-        'click #btnAddItem': function (e, t) {
-            Session.set('list_adding, true');
+        'click #btnAddItem': function (e,t) {
+            Session.set('list_adding', true);
             Meteor.flush();
             focusText(t.find('#item_to_add'));
         },
@@ -152,8 +139,7 @@ if (Meteor.isClient) {
                    
         'keyup #edit_lendee': function (e,t){     
             if (e.which === 13) {
-                updateLendee(Session.get('current_list'),this.Name,       
-                e.target.value);       
+                updateLendee(Session.get('current_list'), this.Name, e.target.value);       
                 Session.set('lendee_input',null);     
             }     
             if (e.which === 27) {       
---
+++ server/kadira.js
@@ -0,0 +1,4 @@
+/**
+ * Created by randre03 on 9/18/14.
+ */
+Kadira.connect('LZiJgySFuidhg3sGh', 'ee0dedde-d1d1-4697-84a9-ea6d6a10dbeb')
